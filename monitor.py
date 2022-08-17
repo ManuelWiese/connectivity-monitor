49e372b42e4a5f17a206e8089d5e900d30c60f32
Manuel Wiese <m.wiese@kaitos.ai>
2022-08-17 12:05:26 +0200

    Use schedule_background and Ping in monitor.py

--- monitor.py
+++ monitor.py
@@ -1,11 +1,13 @@
 import logging
 import random
-import sched
+import threading
 import time
 
-from ping3 import ping
-from prometheus_client import start_http_server, Counter, Gauge, Summary
+from prometheus_client import start_http_server
 import yaml
+
+from ping import Ping
+from schedule_background import schedule_background
 
 
 def main():
@@ -14,11 +16,28 @@ def main():
 
     start_http_server(config['prometheus']['port'])
 
-    scheduler = sched.scheduler(time.time, time.sleep)
+    kill_event = threading.Event()
 
-    ping_schedules = [PingSchedule(server, config['ping']['interval'], scheduler) for server in config['ping']['hosts']]
+    threads = []
 
-    scheduler.run()
+    for host in config['ping']['hosts']:
+        delay = random.random() * config['ping']['random_delay'] if config['ping']['random_delay'] else 0
+        thread = schedule_background(
+            Ping(host),
+            delay=delay,
+            interval=config['ping']['interval'],
+            kill_event=kill_event
+        )
+        threads.append(thread)
+
+    try:
+        for thread in threads:
+            thread.join()
+    except KeyboardInterrupt:
+        logging.info("Shutting down...")
+        kill_event.set()
+        for thread in threads:
+            thread.join()
 
 
 def load_config():
@@ -37,47 +56,6 @@ def configure_logger(logging_config):
         level=level,
         format=format
     )
-
-
-class PingSchedule:
-    def __init__(self, server: str, interval: int, scheduler: sched.scheduler, priority: int = 1):
-        self.server = server
-        self.interval = interval
-        self.scheduler = scheduler
-        self.priority = priority
-
-        self.not_reachable_counter = Counter(
-            f"connectivity_monitor_{self.server.replace('.', '_')}_not_reachable_total",
-            f"Total of times the host {self.server} was not reachable"
-        )
-
-        self.ping_gauge = Gauge(
-            f"connectivity_monitor_{self.server.replace('.', '_')}_ping_seconds",
-            "Ping time of host {self.server} in seconds"
-        )
-
-        start_delay = random.random() * self.interval
-        logging.info(f"Scheduling ping {self.server} in {start_delay}s")
-        self.schedule(start_delay)
-
-    def schedule(self, _time):
-        self.scheduler.enter(
-            _time,
-            self.priority,
-            type(self).run,
-            argument=(self, )
-        )
-
-    def run(self):
-        self.schedule(self.interval)
-        logging.info(f"Pinging {self.server}")
-        result = ping(self.server)
-
-        if result is None:
-            self.not_reachable_counter.inc()
-
-        self.ping_gauge.set(result)
-        logging.info(f"Ping of {self.server}: {result}")
 
 
 if __name__ == "__main__":
